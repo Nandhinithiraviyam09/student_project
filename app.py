ddfd7ca35bd981202f3454dd63d9f0340cc47f3a
Nandhini <nandhinithiraviyam@gmail.com>
2026-05-18 10:52:52 +0530

    Fixed app.py

--- app.py
+++ app.py
@@ -1,61 +1,61 @@
 from flask import Flask, render_template, request, redirect
 import mysql.connector
-from urllib.parse import urlparse
 import os
 
 app = Flask(__name__)
 
-# MYSQL PUBLIC URL
-mysql_url = os.environ.get("MYSQL_PUBLIC_URL")
-
-# PARSE DATABASE URL
-url = urlparse(mysql_url)
-
 # DATABASE CONNECTION
 db = mysql.connector.connect(
-    host=url.hostname,
-    user=url.username,
-    password=url.password,
-    database=url.path[1:],
-    port=url.port
+    host=os.environ.get("MYSQLHOST"),
+    user=os.environ.get("MYSQLUSER"),
+    password=os.environ.get("MYSQLPASSWORD"),
+    database=os.environ.get("MYSQLDATABASE"),
+    port=int(os.environ.get("MYSQLPORT"))
 )
 
-@app.route('/', methods=['GET', 'POST'])
+cursor = db.cursor()
+
+# HOME PAGE
+@app.route('/')
 def home():
 
-    cursor = db.cursor()
-
-    if request.method == 'POST':
-
-        student_name = request.form['student_name']
-        student_email = request.form['student_email']
-        student_course = request.form['student_course']
-
-        sql = """
-        INSERT INTO students(name, email, course)
-        VALUES(%s, %s, %s)
-        """
-
-        values = (
-            student_name,
-            student_email,
-            student_course
-        )
-
-        cursor.execute(sql, values)
-
-        db.commit()
-
-        return redirect('/')
-
+    # FETCH STUDENTS
     cursor.execute("SELECT * FROM students")
-
     students = cursor.fetchall()
 
     return render_template(
         'index.html',
         students=students
     )
+
+# ADD STUDENT
+@app.route('/add', methods=['POST'])
+def add_student():
+
+    name = request.form['name']
+    email = request.form['email']
+    course = request.form['course']
+
+    # INSERT DATA
+    sql = "INSERT INTO students(name, email, course) VALUES (%s, %s, %s)"
+    values = (name, email, course)
+
+    cursor.execute(sql, values)
+    db.commit()
+
+    return redirect('/')
+
+# DELETE STUDENT
+@app.route('/delete/<int:id>')
+def delete_student(id):
+
+    sql = "DELETE FROM students WHERE id=%s"
+    value = (id,)
+
+    cursor.execute(sql, value)
+    db.commit()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
